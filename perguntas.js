e7c661f7f36e1c1d02b49027f588668a6e195c49
Rafaela Cordeiro <144824047+rafaelacordwiro@users.noreply.github.com>
2024-10-25 08:59:46 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -11,13 +11,13 @@ criaCartao(
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Conhecimento',
+    'Quem é Veigh?',
+    'Veigh é um rapper e compositor brasileiro, conhecido por sua habilidade lírica e por abordar temas como a vida nas periferias, desafios pessoais e questões sociais em suas músicas. Ele ganhou notoriedade na cena do rap nacional com seu estilo autêntico e suas letras impactantes. Veigh se destaca por sua versatilidade, misturando influências do trap e do rap tradicional. Com uma base de fãs crescente, ele tem se consolidado como uma das vozes importantes da nova geração do hip-hop no Brasil. Seu trabalho reflete tanto suas experiências pessoais quanto a realidade de muitos jovens nas comunidades.'
 )
 
 criaCartao(
     'Conhecimento',
     'Quem é Matuê?',
-    'Matuê é um rapper e cantor brasileiro, conhecido por seu estilo único e letras que abordam temas como a vida nas periferias, ambições e experiências pessoais. Nascido em Fortaleza, ele ganhou destaque no cenário musical brasileiro a partir de 2019, com lançamentos que misturam trap, rap e elementos de música popular. Matuê é reconhecido por sua autenticidade e pela maneira como se conecta com seu público, sendo uma figura importante na nova geração do rap nacional. Seu álbum "Máquina do Tempo" e outros singles alcançaram grande sucesso, consolidando sua carreira e influênci'
+    'Matuê é um rapper e cantor brasileiro, conhecido por seu estilo único e letras que abordam temas como a vida nas periferias, ambições e experiências pessoais. Nascido em Fortaleza, ele ganhou destaque no cenário musical brasileiro a partir de 2019, com lançamentos que misturam trap, rap e elementos de música popular. Matuê é reconhecido por sua autenticidade e pela maneira como se conecta com seu público, sendo uma figura importante na nova geração do rap nacional. Seu álbum "Máquina do Tempo" e outros singles alcançaram grande sucesso, consolidando sua carreira e influência na música contemporânea.'
 )
